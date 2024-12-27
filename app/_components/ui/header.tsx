21f5857c6005410d8eb3b5c90870f107d7cb5864
Gabriel Castro Do Nascimento <gabrielcdn007@hotmail.com>
2024-12-27 16:31:34 -0300

    feat: add SidebarSheet componente

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -7,67 +7,14 @@ import { quickSearchOption } from "@/app/_constants/search";
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { Avatar } from "./avatar";
 import Link from "next/link";
+import SidebarButton from "./sidebar-button";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 flex flex-row items-center justify-between">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <SheetContent className="overflow-y-auto">
-            <SheetHeader>
-              <SheetTitle className="text-left">Menu</SheetTitle>
-            </SheetHeader>
-            
-            <div className="flex items-center gap-3 border-b border-solid py-5">
-                <Avatar>
-                    <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww"></AvatarImage>
-                </Avatar>
-                <div>
-                    <p className="font-bold">Gabriel Nascimento</p>
-                    <p className="text-xs">gabrielcdn077@gmail.com</p>
-                </div>
-            </div>
-            <div className="flex flex-col gap-1 py-5 boder-b border-solid">
-                <SheetClose asChild>
-              <Button className="justify-start gap-2" variant="ghost" asChild>
-                <Link href="/">
-                <HomeIcon size={18} />
-                Início 
-                </Link>
-              </Button>
-              </SheetClose>
-              <Button className="justify-start gap-2" variant="ghost">
-                <CalendarIcon size={18} /> 
-                Agendamento</Button>
-            </div>
-
-            <div className="flex flex-col gap-1 py-5 boder-b border-solid">
-                {quickSearchOption.map((option) => (
-                <Button key={option.title} className="justify-start gap-2"
-                variant="ghost">
-                    <Image alt={option.title} src={option.imageUrl} height={18} width={18} />
-                    {option.title}
-                    
-                </Button>
-
-                ))}
-            </div>
-
-
-            <div className="flex flex-col gap-2 py-5">
-                <Button variant="ghost" className="justify-start gap-2">
-                <LogOutIcon size={18} />Sair da conta</Button>
-            </div>
-
-          </SheetContent>
-        </Sheet>
+        <SidebarButton />
       </CardContent>
     </Card>
   );
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import PhoneItem from "@/app/_components/phone-item";
 import ServiceItem from "@/app/_components/service-item";
 import { Button } from "@/app/_components/ui/button";
+import Header from "@/app/_components/ui/header";
+import SidebarButton from "@/app/_components/ui/sidebar-button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image"
@@ -43,7 +45,7 @@ const BarbershopPage = async ({params}: BarbershopPageProps) => {
             </Button>   
 
             <Button size="icon" variant="secondary" className="absolute right-4 top-4">
-                <MenuIcon />
+                <SidebarButton />
             </Button>
             </div>
 
